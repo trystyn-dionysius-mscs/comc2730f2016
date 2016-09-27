ee2c58ebd26439055ae0d4ac224bd3fecc87d40a
Dionysius <Dionysius@DIONYSIUS-PC>
2016-09-27 15:00:06 -0500

    Added 1g

---
+++ WindowsApplication1/Main Form.vb
@@ -0,0 +1,19 @@
+﻿' Name:  Average Project
+' Purpose: Displays the average of 3 test scores
+Public Class frmAvgcalc
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        lblAvgScore.Text = Val(txtScore1.Text) + (txtScore2.Text) + (txtScore3.Text) / 3
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        lblAvgScore.Text = String.Empty
+        txtScore1.Text = String.Empty
+        txtScore2.Text = String.Empty
+        txtScore3.Text = String.Empty
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
---
+++ WindowsApplication1/tdionysius1g1.Designer.vb
@@ -0,0 +1,168 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmAvgcalc
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblTst1 = New System.Windows.Forms.Label()
+        Me.lblTst2 = New System.Windows.Forms.Label()
+        Me.lblTst3 = New System.Windows.Forms.Label()
+        Me.txtScore1 = New System.Windows.Forms.TextBox()
+        Me.txtScore2 = New System.Windows.Forms.TextBox()
+        Me.txtScore3 = New System.Windows.Forms.TextBox()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblAvgScore = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblTst1
+        '
+        Me.lblTst1.AutoSize = True
+        Me.lblTst1.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTst1.Location = New System.Drawing.Point(27, 69)
+        Me.lblTst1.Name = "lblTst1"
+        Me.lblTst1.Size = New System.Drawing.Size(89, 20)
+        Me.lblTst1.TabIndex = 0
+        Me.lblTst1.Text = "Test score &1:"
+        '
+        'lblTst2
+        '
+        Me.lblTst2.AutoSize = True
+        Me.lblTst2.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTst2.Location = New System.Drawing.Point(27, 115)
+        Me.lblTst2.Name = "lblTst2"
+        Me.lblTst2.Size = New System.Drawing.Size(89, 20)
+        Me.lblTst2.TabIndex = 1
+        Me.lblTst2.Text = "Test score &2:"
+        '
+        'lblTst3
+        '
+        Me.lblTst3.AutoSize = True
+        Me.lblTst3.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTst3.Location = New System.Drawing.Point(27, 157)
+        Me.lblTst3.Name = "lblTst3"
+        Me.lblTst3.Size = New System.Drawing.Size(85, 20)
+        Me.lblTst3.TabIndex = 2
+        Me.lblTst3.Text = "Test score&3:"
+        '
+        'txtScore1
+        '
+        Me.txtScore1.Location = New System.Drawing.Point(122, 69)
+        Me.txtScore1.Name = "txtScore1"
+        Me.txtScore1.Size = New System.Drawing.Size(55, 20)
+        Me.txtScore1.TabIndex = 3
+        '
+        'txtScore2
+        '
+        Me.txtScore2.Location = New System.Drawing.Point(122, 115)
+        Me.txtScore2.Name = "txtScore2"
+        Me.txtScore2.Size = New System.Drawing.Size(55, 20)
+        Me.txtScore2.TabIndex = 4
+        '
+        'txtScore3
+        '
+        Me.txtScore3.Location = New System.Drawing.Point(122, 159)
+        Me.txtScore3.Name = "txtScore3"
+        Me.txtScore3.Size = New System.Drawing.Size(55, 20)
+        Me.txtScore3.TabIndex = 5
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(183, 66)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalculate.TabIndex = 6
+        Me.btnCalculate.Text = "&Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(183, 115)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 7
+        Me.btnClear.Text = "&Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(183, 157)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 8
+        Me.btnExit.Text = "&Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(27, 208)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(134, 20)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "Average test score:"
+        '
+        'lblAvgScore
+        '
+        Me.lblAvgScore.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblAvgScore.Location = New System.Drawing.Point(167, 208)
+        Me.lblAvgScore.Name = "lblAvgScore"
+        Me.lblAvgScore.Size = New System.Drawing.Size(60, 23)
+        Me.lblAvgScore.TabIndex = 10
+        '
+        'frmAvgcalc
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.lblAvgScore)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.txtScore3)
+        Me.Controls.Add(Me.txtScore2)
+        Me.Controls.Add(Me.txtScore1)
+        Me.Controls.Add(Me.lblTst3)
+        Me.Controls.Add(Me.lblTst2)
+        Me.Controls.Add(Me.lblTst1)
+        Me.Name = "frmAvgcalc"
+        Me.Text = "Average Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblTst1 As Label
+    Friend WithEvents lblTst2 As Label
+    Friend WithEvents lblTst3 As Label
+    Friend WithEvents txtScore1 As TextBox
+    Friend WithEvents txtScore2 As TextBox
+    Friend WithEvents txtScore3 As TextBox
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblAvgScore As Label
+End Class
